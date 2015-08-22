0e9b8f50fe27fc8aef8e8181e2eed252c79de79b
Aram Zadikian <brancusi@gmail.com>
2015-08-22 14:46:23 -0700

    Adding some goodies

--- app/pods/components/word-stash/component.js
+++ app/pods/components/word-stash/component.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['wordStash']
+  classNames: ['wordStash'],
+
+  formattedName: Ember.computed(function(){
+    return this.get('type').toUpperCase();
+  })
+
 });
