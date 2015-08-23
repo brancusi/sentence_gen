2cdf239f0fcf92f65798572e4e23edb6177bef47
Luciano Zadikian <dudeious21@gmail.com>
2015-08-23 14:10:33 -0700

    added text field

--- app/pods/components/word-stash/component.js
+++ app/pods/components/word-stash/component.js
@@ -3,8 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['wordStash'],
 
+  words: Ember.A(),
+
   formattedName: Ember.computed(function(){
     return this.get('type').toUpperCase();
-  })
+  }),
 
+  actions: {
+  	handleEnter: function(){
+  		this.get('words').pushObject(this.get('newName'));
+  	}
+  }
 });
